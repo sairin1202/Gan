bb4ee76f0c8145aceb487711451bfc89085cf27c
sairin1202 <sairin1202@gmail.com>
2018-07-17 15:40:37 +0900

    delete cache

--- gan/demo/convert_gif.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import imageio,os
-images = []
-filenames=[]
-for i in range(100):
-    filenames.append("demo_Epoch"+str(i)+".jpg")
-for filename in filenames:
-    images.append(imageio.imread(filename))
-imageio.mimsave('source.gif', images,duration=0.2)
--- gan/reset.py
+++
@@ -1,3 +0,0 @@
-open('disLoss.txt', 'w').close()
-open('genLoss.txt', 'w').close()
-
